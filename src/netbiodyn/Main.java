f41142fb7942eba356d616125624eea277e92ca1
Emeline Duquenne <eduque910e@emi.u-bordeaux1.fr>
2015-12-07 18:35:19 +0100

    Main commenté

--- src/netbiodyn/Main.java
+++ src/netbiodyn/Main.java
@@ -2,9 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package netbiodyn;
 
-import netbiodyn.ihm.Controller;
+package netbiodyn; //On crée un package, cette ligne indiquant que le fichier actuel sera dans ce package 
+
+import netbiodyn.ihm.Controller; //On importe dans le Main le Contrôleur dont on va créer un objet dans la suite du code.
 
 /**
  *
@@ -12,6 +13,7 @@ import netbiodyn.ihm.Controller;
  */
 public class Main {
 
+	//taille d'origine de la fenêtre du logiciel
     static int FRAME_WIDTH = 900;
     static int FRAME_HEIGHT = 600;
 
@@ -19,6 +21,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args){
+    	//l'ouverture du logiciel entraîne la création du Contrôleur (dans ihm)
         new Controller();
     }
 
